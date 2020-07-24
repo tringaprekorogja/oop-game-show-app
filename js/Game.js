636c013c28fc8a80c59a0249fecc700dcdded32c
tringa-prekorogja <t.prekorogja@gmail.com>
2020-07-24 16:54:13 +0200

    Add classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     constructor(){
+         
+     }
+
+ }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+const phrase = new Phrase();
+const game = new Game();
