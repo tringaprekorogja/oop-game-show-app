0551b37c432dd4aa63605289bbd76553c57e2a4d
tringa-prekorogja <t.prekorogja@gmail.com>
2020-07-24 20:06:21 +0200

    Create phrases

--- js/Game.js
+++ js/Game.js
@@ -2,11 +2,25 @@
  * Project 4 - OOP Game App
  * Game.js */
 
- class Game {
-     constructor(){
-         this.missed = 0;
-         this.phrases = [];
-         this.activePhrase = null;
-     }
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases() {
+        const phrases = [
+            {phrase:'An apple a day keeps the doctor away'},
+            {phrase: 'A change is as good as a rest'},
+            {phrase: 'Absolute power corrupts absolutely'},
+            {phrase: 'Actions speak louder than words'},
+            {phrase: 'It is better to light a candle than curse the darkness'}
+        ];
+        return phrases
+    }
 
- }
+}
--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-//onst phrase = new Phrase();
-const game = new Game();
 
-const phrase = new Phrase('Life is like a box of chocolates');
-console.log(`Phrase - phrase: ${phrase.phrase}`);
