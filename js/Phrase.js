1c148962f84c21e7ef4854782402feb3ff196448
tringa-prekorogja <t.prekorogja@gmail.com>
2020-07-25 10:08:10 +0200

    Add letter placeholders to display

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,9 +2,31 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 
- class Phrase {
-     constructor(phrase){
-         this.phrase = phrase.toLowerCase();
-     }
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+    /**
+   * Display phrase on game board
+   */
+    addPhraseToDisplay() {
+        const phraseUl = document.querySelector('#phrase ul')
 
- }
+        for (let i = 0; i < this.phrase.length; i++) {
+            let letterListItem = document.createElement('LI');
+            letterListItem.textContent = this.phrase[i];
+
+            phraseUl.appendChild(letterListItem)
+
+            if (this.phrase[i] === ' ') {
+                letterListItem.setAttribute('class', 'space')
+            } else {
+                letterListItem.setAttribute('class', 'hide letter ' + this.phrase[i])
+            }
+
+
+
+
+        }
+    }
+    }
--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@
  * Project 4 - OOP Game App
  * app.js */
 
+const game = new Game();
+const randomPhrase = game.getRandomPhrase();
+const phrase = new Phrase(randomPhrase.phrase);
+phrase.addPhraseToDisplay();
+
+
